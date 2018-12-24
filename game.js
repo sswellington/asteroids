f9a9cff1838c95d4af060bfc68c555a43e9a933c
sswellington <wellingtondessilva@gmail.com>
2018-12-24 18:15:19 -0200

    Make asteroids split into 2 smaller pieces instead of 3 and increased speed

--- game.js
+++ game.js
@@ -690,10 +690,10 @@ Asteroid = function () {
   this.collidesWith = ["ship", "bullet", "bigalien", "alienbullet"];
 
   this.breakIntoFragments = function () {
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 5; i++) {
       var roid = $.extend(true, {}, this);
-      roid.vel.x = Math.random() * 6 - 3;
-      roid.vel.y = Math.random() * 6 - 3;
+      roid.vel.x = Math.random() * 6 - 4;
+      roid.vel.y = Math.random() * 6 - 4;
       if (Math.random() > 0.5) {
         roid.points.reverse();
       }
